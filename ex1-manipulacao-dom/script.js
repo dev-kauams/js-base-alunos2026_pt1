e61c1d246d83eb04272b213b6315acda74e3ec18
dev-kauams <kauams.profissional@gmail.com>
2026-03-05 08:32:08 -0300

    commit v1.0
    Resolução Exercício 1.

--- ex1-manipulacao-dom/script.js
+++ ex1-manipulacao-dom/script.js
@@ -1,8 +1,12 @@
 // Seleciona o elemento do botão no DOM usando o ID "myButton" e armazena na variável 'button'
-
+const myButton = document.querySelector('button')
 // 3ª Digitação (Aqui) 
 
 // Seleciona o elemento de parágrafo no DOM usando o ID "myText" e armazena na variável 'text'
-
+let myText = document.getElementById('pText')
 
 // Adiciona um "ouvinte" de evento ao botão. Quando o botão for clicado, a função dentro do 'addEventListener' será executada
+
+myButton.addEventListener("click", () =>{
+    myText.textContent = "Olá, Mundo!"
+})
